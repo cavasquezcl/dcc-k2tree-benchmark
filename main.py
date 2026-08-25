4e4d2e7b714b9cd89175f0469e1e6635b2c7b14a
Carlos A. Vásquez <ca.vasquez.cl@gmail.com>
2026-08-24 21:49:41 -0400

    feat: Hola mundo

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.k2tree import K2Tree
+
+print("inicia main.py \n")
+
+# instancia K2Tree 
+arbol = K2Tree()
+
+print("fin desde main.py \n")
---
+++ src/k2tree.py
@@ -0,0 +1,3 @@
+class K2Tree:
+    def __init__(self):
+        print("init de K2-Tree \n")
