63e516151abaf4edb2ced91a96588b1f6e83a908
Carlos A. Vásquez <ca.vasquez.cl@gmail.com>
2026-08-27 17:48:41 -0400

    refactor: Se transforma la implementación de CSR en clase
    Esto es para estandarizar con el estilo de k2-tree y luego se instancia en el main.py, y ahí se realizan las pruebas.

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
+import time
+
 from src.carga_dataset import cargar, RUTA_DATASET
 from src.k2tree import K2Tree
+from src.csr import CSR
 
 print("inicia main.py \n")
 
 filas, columnas, n = cargar(RUTA_DATASET)
 
+
+
+# instancia CSR
+csr = CSR(filas, columnas, n)
+
+inicio = time.perf_counter()
+csr.existe_arista_lineal(1, 17793)
+fin = time.perf_counter()
+print("\ntiempo existe_arista_lineal:", (fin - inicio))
+
+inicio = time.perf_counter()
+csr.existe_arista_binaria(1, 17793)
+fin = time.perf_counter()
+print("tiempo existe_arista_binaria:", (fin - inicio))
+
+print("\nbits_numpy():", csr.bits_numpy())
+print("bits_numpy() / m:", csr.bits_numpy() / len(csr.indices))
+print("bits_estructura():", csr.bits_estructura())
+print("bits_estructura() / m:", csr.bits_estructura() / len(csr.indices))
+
+
+
+
 # instancia K2Tree
 arbol = K2Tree(filas, columnas, n, 2)
 
-print("fin desde main.py \n")
+
+
+print("\n\nfin desde main.py \n")
--- src/csr.py
+++ src/csr.py
@@ -1,83 +1,67 @@
 import math
-import time
 import numpy as np
-from src.carga_dataset import cargar, RUTA_DATASET
 
-if __name__ == "__main__":
-    filas, columnas, n = cargar(RUTA_DATASET)
+class CSR:
+    def __init__(self, filas, columnas, n):
+        print("\ninit de CSR")
+        self.filas = filas
+        self.columnas = columnas
+        self.n = n
 
-    print("aristas:", len(filas))
+        # la representación
+        self.indptr = None
+        self.indices = None
 
-    # verifica que no hayan self-loops ni arists repetidas (dataset ad-hoc)
-    pares = np.column_stack([filas, columnas])
+        # construye
+        self.construir()
 
-    # revisa que no hayan self-loops, debería dar 0
-    cant_self_loops = (pares[:, 0] == pares[:, 1]).sum()
-    print("\nself-loops:", cant_self_loops)
+    def construir(self):
 
-    # revisa que no hayan repetidos, debería dar 0
-    cant_repetidos = len(pares) - len(np.unique(pares, axis=0))
-    print("\naristas repetidas:", cant_repetidos)
+        filas = self.filas
+        columnas = self.columnas
+        n = self.n
 
-    # arma el csr
+        # verifica que no hayan self-loops ni arists repetidas (dataset ad-hoc)
+        pares = np.column_stack([filas, columnas])
 
-    orden = np.lexsort((columnas, filas))  # ordena por filas y columnas
-    filas = filas[orden]
-    columnas = columnas[orden]
+        # revisa que no hayan self-loops, debería dar 0
+        cant_self_loops = (pares[:, 0] == pares[:, 1]).sum()
+        print("\nself-loops:", cant_self_loops)
 
-    grado = np.bincount(filas, minlength=n)  # cuenta apariciones
-    indptr = np.zeros(n + 1, dtype=np.int64)  # array lleno de 0s para contador
-    indptr[1:] = np.cumsum(grado)  # suma cuantas aristas tiene cada nodo
+        # revisa que no hayan repetidos, debería dar 0
+        cant_repetidos = len(pares) - len(np.unique(pares, axis=0))
+        print("\naristas repetidas:", cant_repetidos)
 
-    indices = columnas
+        # arma el csr
 
-    print("\nn:", n)
-    print("\naristas csr:", len(indices))
+        orden = np.lexsort((columnas, filas))  # ordena por filas y columnas
+        filas = filas[orden]
+        columnas = columnas[orden]
 
-    def vecinos(nodo):
-        return indices[indptr[nodo]:indptr[nodo + 1]]
+        grado = np.bincount(filas, minlength=n)  # cuenta apariciones
+        indptr = np.zeros(n + 1, dtype=np.int64)  # array lleno de 0s para contador
+        indptr[1:] = np.cumsum(grado)  # suma cuantas aristas tiene cada nodo
 
-    print("\nvecinos(1):", vecinos(1))
-    print("vecinos(2):", vecinos(2))
+        # guarda la estructura
+        self.indptr = indptr
+        self.indices = columnas
 
-    def existe_arista_lineal(nodo_origen, nodo_destino):
-        return nodo_destino in vecinos(nodo_origen)
+    def vecinos(self, nodo):
+        return self.indices[self.indptr[nodo]:self.indptr[nodo + 1]]
 
-    def existe_arista_binaria(nodo_origen, nodo_destino):
-        vecinos_origen = vecinos(nodo_origen)
+    def existe_arista_lineal(self, nodo_origen, nodo_destino):
+        return nodo_destino in self.vecinos(nodo_origen)
+
+    def existe_arista_binaria(self, nodo_origen, nodo_destino):
+        vecinos_origen = self.vecinos(nodo_origen)
         pos_v_origen = np.searchsorted(vecinos_origen, nodo_destino)
         return pos_v_origen < len(vecinos_origen) and vecinos_origen[pos_v_origen] == nodo_destino
 
-    print("\nexiste_arista_lineal(1, 17793):", existe_arista_lineal(1, 17793))
-    print("existe_arista_binaria(1, 17793):", existe_arista_binaria(1, 17793))
-    print("existe_arista_lineal(1, 0):", existe_arista_lineal(1, 0))
-    print("existe_arista_binaria(1, 0):", existe_arista_binaria(1, 0))
-    print("existe_arista_lineal(2, 74360):", existe_arista_lineal(2, 74360))
-    print("existe_arista_binaria(2, 74360):", existe_arista_binaria(2, 74360))
+    def bits_numpy(self):
+        return (self.indptr.nbytes + self.indices.nbytes) * 8
 
-    # prueba de tiempo
-
-    inicio = time.perf_counter()
-    existe_arista_lineal(1, 17793)
-    fin = time.perf_counter()
-    print("\ntiempo existe_arista_lineal:", (fin - inicio))
-
-    inicio = time.perf_counter()
-    existe_arista_binaria(1, 17793)
-    fin = time.perf_counter()
-    print("tiempo existe_arista_binaria:", (fin - inicio))
-
-    def bits_numpy():
-        return (indptr.nbytes + indices.nbytes) * 8
-
-    m = len(indices)  # cantidad de aristas
-
-    def bits_estructura():
+    def bits_estructura(self):
+        m = len(self.indices)  # cantidad de aristas
         bits_por_indptr = math.ceil(math.log2(m + 1))
-        bits_por_indice = math.ceil(math.log2(n))
-        return (n + 1) * bits_por_indptr + m * bits_por_indice
-
-    print("\nbits_numpy():", bits_numpy())
-    print("bits_numpy() / m:", bits_numpy() / m)
-    print("bits_estructura():", bits_estructura())
-    print("bits_estructura() / m:", bits_estructura() / m)
+        bits_por_indice = math.ceil(math.log2(self.n))
+        return (self.n + 1) * bits_por_indptr + m * bits_por_indice
